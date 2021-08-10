14f943bee5fe9efe95fafc7a037e5b1770a3859e
ben <rotted_ben@protonmail.com>
2021-08-10 10:03:16 +0200

    stuff

--- src/widgets/Menu/index.stories.tsx
+++ src/widgets/Menu/index.stories.tsx
@@ -24,7 +24,7 @@ export const Connected: React.FC = () => {
         account="0xbdda50183d817c3289f895a4472eb475967dc980"
         login={noop}
         logout={noop}
-        isDark={false}
+        isDark={true}
         toggleTheme={noop}
         langs={langs}
         setLang={noop}
